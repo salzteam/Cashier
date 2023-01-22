d8ef30fadab758d39c9c9babc9e25461abfd3a54
salzteam <akshalrizkigandari@gmail.com>
2023-01-22 10:27:57 +0700

    add middlewares

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -3,13 +3,9 @@ const transactionsRouter = require("express").Router();
 const transactionsController = require("../controllers/transactions");
 const { create, listTranscation, listTranscationUsers } =
   transactionsController;
-// const validate = require("../middlewares/validate");
-// const isLogin = require("../middlewares/isLogin");
+const validate = require("../middlewares/validate");
 
 transactionsRouter.get("/", listTranscation);
-transactionsRouter.get("/byUser", listTranscationUsers);
-transactionsRouter.post("/create", create);
-// transactionsRouter.delete("/", isLogin(), logout);
-// transactionsRouter.patch("/", validate.params("email", "otp", "new_password"), forgot);
-
+transactionsRouter.get("/byUser", validate.params("id"), listTranscationUsers);
+transactionsRouter.post("/create", validate.body("userid", "product"), create);
 module.exports = transactionsRouter;
